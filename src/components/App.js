e172e734ba882697d3d9e10d2a73abd001701c2e
Ermin Cahtarevic <erminc@windowslive.com>
2020-06-06 02:01:35 +0200

    Add app.css file to solve stylelint issues

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
+import '../styles/app.css';
 
 const App = () => (
   <div>
