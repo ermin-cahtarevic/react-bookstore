f30b51f13a5a446074d0a792eb465b42ea0897db
Ermin Cahtarevic <erminc@windowslive.com>
2020-06-09 21:52:10 +0200

    Add styling to the header section

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import categories from '../helpers/categories';
+import '../styles/category-filter.css';
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const filters = ['All', ...categories()];
 
 const CategoryFilter = ({ changeFilter }) => (
-  <div>
-    <select onChange={e => changeFilter(e.target.value)}>
-      {
-        filters.map(filter => (
-          <option key={filter} value={filter}>{filter}</option>
-        ))
-      }
-    </select>
+  <div className="header">
+    <div className="header-left">
+      <div className="logo">Bookstore CMS</div>
+      <div className="header-books">BOOKS</div>
+      <select className="select" onChange={e => changeFilter(e.target.value)}>
+        {
+          filters.map(filter => (
+            <option key={filter} value={filter}>{filter}</option>
+          ))
+        }
+      </select>
+    </div>
+    <div className="user-icon">
+      <FontAwesomeIcon
+        icon={faUser}
+        style={{
+          color: "#0290ff"
+        }}
+      />
+    </div>
   </div>
 );
 
