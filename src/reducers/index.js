16c9ce7c89031d168439aeea52ded008d49241dc
Ermin Cahtarevic <erminc@windowslive.com>
2020-06-09 16:31:07 +0200

    Create filter reducer, add it to the main reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import books from './books';
+import filterReducer from './filter';
 
 const reducer = initialState => {
   const booksReducer = books(initialState);
   return combineReducers({
     booksReducer,
+    filterReducer,
   });
 };
 
